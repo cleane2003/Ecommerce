8110fc166d7c371e7ef866aaa1f7254dfc99daf2
Cleane Batista <cleaneevelin04@gmail.com>
2023-05-24 21:41:41 -0300

    feat: Gerenciamento de perfis de usuários

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,49 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import AppError from '@shared/errors/AppError';
+import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const { token } = await userTokensRepository.generate(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgotPassword.hbs',
+    );
+
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/resetPassword?token=${token}`,
+        },
+      },
+    });
+  }
+}
+
+export default SendForgotPasswordEmailService;
