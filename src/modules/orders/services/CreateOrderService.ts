fd0e6544948d56f62577558ae1b0411e0055ae02
cleane batista <cleaneevelin04@gmail.com>
2023-06-01 15:43:10 -0300

    feat: adicionando novas funcionalidades

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from 'typeorm';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new Error('There is already one product with this name');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
